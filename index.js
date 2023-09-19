01791e632fd72726cefa6b9afb90be383860a86c
luigiolivi <eterowiczluigi@gmail.com>
2023-09-19 20:10:17 -0300

    possibilidades() fixed and transcricao() structure

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const valores = [[6, 2]]
+const valores = [6, 2, 2]
+const nomes = [[1, 2, 3, 4, 5, 6], ['cara', 'coroa'], ['cara', 'coroa']]
 
 const possibilidades = () => {
     let resultados = []
+
+    let possibilidades = 1
     for(let i = 0; i < valores.length; i++) {
-        for(let j = 0; j < valores[i].length; j++) {
-            let possibilidades = 1
-            possibilidades *= valores[i][j]
-        }
-        resultados.push(this.possibilidades)
+        possibilidades *= valores[i]
     }
+    resultados.push(possibilidades)
 
     return resultados
+}
+
+const transcricao = () => {
+    let resultado = []
+    for(let i = 0; i < nomes.length; i++) {
+        for(let j = 0; j < nomes[i].length; j++) {
+            
+        }
+    }    
 }
 
 console.log(possibilidades())
